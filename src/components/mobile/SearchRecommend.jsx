ade2b8f074de092a9032a316e25a7af54856f16d
Aidan <aidankmcalister@gmail.com>
2023-11-14 21:39:15 -0600

    search complete

--- src/components/mobile/SearchRecommend.jsx
+++ src/components/mobile/SearchRecommend.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { fetchRecommendedMoviesAndShows } from "../../api/api";
 import { PlayCircleIcon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
 
 const SearchRecommend = () => {
   const [recommendedMedia, setRecommendedMedia] = useState([]);
@@ -22,16 +23,23 @@ const SearchRecommend = () => {
       <h1 className="font-semibold text-lg">Recommended TV Shows & Movies</h1>
       <div>
         {recommendedMedia.map((media, index) => (
-          <div key={index} className="flex w-full mb-4">
-            <img
-              src={`http://image.tmdb.org/t/p/w500/${media.backdrop_path}`}
-              className="w-1/2"
-            />
-            <div className="flex justify-between items-center w-full">
-              <h3 className="font-bold ml-4">{media.name || media.title}</h3>
-              <PlayCircleIcon className="w-8" />
+          <Link
+            to={{
+              pathname: `/content/${media.id}`,
+              search: `?mediaObject=${JSON.stringify(media)}`,
+            }}
+          >
+            <div key={index} className="flex w-full mb-4">
+              <img
+                src={`http://image.tmdb.org/t/p/w500/${media.backdrop_path}`}
+                className="w-1/2"
+              />
+              <div className="flex justify-between items-center w-full">
+                <h3 className="font-bold ml-4">{media.name || media.title}</h3>
+                <PlayCircleIcon className="w-8" />
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
--- src/components/mobile/SearchResults.jsx
+++ src/components/mobile/SearchResults.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { PlayCircleIcon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
 
 const SearchResults = ({ results }) => {
   return (
@@ -8,21 +8,27 @@ const SearchResults = ({ results }) => {
       <div>
         {results.map((result, index) => {
           return !result.backdrop_path ? (
-            <div key={index} key={index}></div>
+            <div key={index}></div>
           ) : (
-            <div key={index} className="flex w-full mb-4">
-              <img
-                src={`http://image.tmdb.org/t/p/w500/${result.backdrop_path}`}
-                className="w-1/2"
-                alt={result.name || result.title}
-              />
-              <div className="flex justify-between items-center w-full">
-                <h3 className="font-bold ml-4">
-                  {result.name || result.title}
-                </h3>
-                <PlayCircleIcon className="w-8" />
+            <Link
+              to={{
+                pathname: `/content/${result.id}`,
+                search: `?mediaObject=${JSON.stringify(result)}`,
+              }}
+            >
+              <div key={index} className="flex w-full mb-4">
+                <img
+                  src={`http://image.tmdb.org/t/p/w500/${result.backdrop_path}`}
+                  className="w-1/2"
+                  alt={result.name || result.title}
+                />
+                <div className="flex justify-between items-center w-full">
+                  <h3 className="font-bold ml-4">
+                    {result.name || result.title}
+                  </h3>
+                </div>
               </div>
-            </div>
+            </Link>
           );
         })}
       </div>
